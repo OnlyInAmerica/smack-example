847005f536e10c32d4319a526f40612d50ec1a01
David Brodsky <dbro@dbro.pro>
2014-05-16 21:45:55 -0700

    Dummy send message loop until message received
    Tested importing all smack modules

--- smack-example/src/main/java/pro/dbro/smack/Client.java
+++ smack-example/src/main/java/pro/dbro/smack/Client.java
@@ -11,17 +11,37 @@ import java.io.IOException;
  */
 public class Client {
 
-    public static void main(String [] args) throws IOException, XMPPException, SmackException {
-        XMPPConnection connection = new XMPPTCPConnection("jabber.org");
-        connection.connect();
-        connection.login("mtucker", "password");
-        Chat chat = ChatManager.getInstanceFor(connection)
-                .createChat("jsmith@jivesoftware.com", new MessageListener() {
+    private static final Object mMessageLock = new Object();
+    private static boolean mGotMsg = false;
 
-                    public void processMessage(Chat chat, Message message) {
-                        System.out.println("Received message: " + message);
-                    }
-                });
-        chat.sendMessage("Howdy!");
+    public static void main(String [] args) throws IOException, XMPPException, SmackException, InterruptedException {
+        ConnectionConfiguration config = new ConnectionConfiguration(SECRETS.XMPP_SERVER, SECRETS.XMPP_PORT, SECRETS.XMPP_SERVICE);
+        XMPPConnection connection = new XMPPTCPConnection(config);
+        connection.connect();
+        connection.login(SECRETS.XMPP_USER, SECRETS.XMPP_PASS);
+
+        synchronized (mMessageLock) {
+            Chat chat = ChatManager.getInstanceFor(connection)
+                    .createChat(SECRETS.XMPP_RECIPIENT, new MessageListener() {
+
+                        public void processMessage(Chat chat, Message message) {
+                            System.out.println("Received message: " + message);
+                            mGotMsg = true;
+                            try {
+                                chat.sendMessage("Just kidding! See ya later!");
+                            } catch (XMPPException | SmackException.NotConnectedException e) {
+                                e.printStackTrace();
+                            }
+                            synchronized (mMessageLock) {
+                                mMessageLock.notify();
+                            }
+                        }
+                    });
+            while (!mGotMsg) {
+                chat.sendMessage("Howdy!");
+                System.out.println("Waiting for msg");
+                mMessageLock.wait(10 * 1000);
+            }
+        }
     }
 }
